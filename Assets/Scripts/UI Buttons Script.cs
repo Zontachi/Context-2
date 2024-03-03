4e2f9797d1e12890de2681267ab825e93efbd4aa
Pascal <Zontachi@gmail.com>
2024-03-03 18:09:42 +0100

    Project Upload

---
+++ Assets/Scripts/UI Buttons Script.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("Button Clicked!");
+        // Add your button click logic here
+    }
+}
