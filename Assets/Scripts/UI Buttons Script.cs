af14c66f13d9ff2845a3337a94f8cff865a25912
Pascal <Zontachi@gmail.com>
2024-03-21 12:39:32 +0100

    Map functionality
    Map now contains interactable buttons

--- Assets/Scripts/UI Buttons Script.cs
+++ Assets/Scripts/UI Buttons Script.cs
@@ -1,11 +1,42 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonHandler : MonoBehaviour
 {
-    public void OnButtonClick()
+    public void TrainClicked()
     {
-        Debug.Log("Button Clicked!");
-        // Add your button click logic here
+        Debug.Log("Loading new area");
+        //SceneManager.LoadScene(sceneName);
+    }
+    public void BuildingClicked()
+    {
+        Debug.Log("Loading new area");
+        //SceneManager.LoadScene(sceneName);
+    }
+    public void HouseClicked()
+    {
+        Debug.Log("Loading new area");
+        //SceneManager.LoadScene(sceneName);
+    }
+    public void BathClicked()
+    {
+        Debug.Log("Loading new area");
+        //SceneManager.LoadScene(sceneName);
+    }
+    public void TreeClicked()
+    {
+        Debug.Log("Loading new area");
+        //SceneManager.LoadScene(sceneName);
+    }
+    public void TentClicked()
+    {
+        Debug.Log("Loading new area");
+        //SceneManager.LoadScene(sceneName);
+    }
+    public void ArenaClicked()
+    {
+        Debug.Log("Loading new area");
+        //SceneManager.LoadScene(sceneName);
     }
 }
